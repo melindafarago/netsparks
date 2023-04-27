b5042aceb5bca9995cd314b85f9e5ee81afa1518
Faragó Melinda <melinda.farago@pazirik.hu>
2023-04-27 13:34:09 +0200

    fetch data

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,27 @@ let show = false;
 const showContent = () => show=!show;
 const toogleButton = document.getElementById("toogleButton");
 
+let dataList=[];
+
+/*function fetchData(){
+    fetch("/data/data.json")
+    .then(data => data.json())
+    .then(data =>{
+        
+        dataList=data;
+        console.log(dataList);
+        renderData();
+    })
+}*/
+
+async function fetchData(){
+    let result = await fetch("/data/data.json");
+    dataList = await result.json();
+    console.log(dataList);
+   
+}
+
+
 function toogle(){
    
 }
