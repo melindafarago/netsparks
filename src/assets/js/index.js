eb9daae151b9c6cc64aaaf3b84ef76bb7954bdfe
Faragó Melinda <melinda.farago@pazirik.hu>
2023-04-27 14:20:31 +0200

    show radnom event, when click show button

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 let show = false;
 const showContent = () => show=!show;
 const toogleButton = document.getElementById("toogleButton");
+const itemContainer = document.getElementById("item-container");
 
 let dataList=[];
 
@@ -25,6 +26,7 @@ async function fetchData(){
 
 function toogle(){
     if (showContent()){
+        renderData();
         anime({
            targets: ['#item-container'],
            opacity: '1',
@@ -34,6 +36,7 @@ function toogle(){
          });
        toogleButton.innerHTML="Hide";
    } else {
+       
        anime({
            targets: ['#item-container'],
            opacity: '0',
@@ -42,12 +45,43 @@ function toogle(){
            loop: false
          });
        toogleButton.innerHTML="Show";
+
    }
+}
+function getMonthName(d){
+    const date = new Date(d);  //use Internatializatin API to get month name
+    const month = date.toLocaleString('en', { month: 'long' });
+   return month;
+}
+function renderData(){
+    const random = Math.floor(Math.random() * dataList.length);
+    const currEvent= dataList[random];
+   
+    const backgroundColor = currEvent["color"];
+    const monthNameFrom = getMonthName(currEvent["from"]);
+    const monthNameTo = getMonthName(currEvent["to"]);
+    const dayFrom = new Date(currEvent["from"]).getDate();
+    const dayTo = new Date(currEvent["to"]).getDate();
+    let talenList=``;
+    const talentArray= currEvent["talent"];
+
+    talentArray.map(talent =>{
+        console.log(talent);
+        talenList+=`<div style="background : ${backgroundColor}">${talent}</div>`
+    })
+    
+    const content = ` <div id="name">${currEvent["name"]}</div>
+                        <div id="date">${monthNameFrom} ${dayFrom} - ${monthNameTo} ${dayTo}</div>
+                        <div id="talent">
+                            ${talenList}
+                        </div>`;
+   itemContainer.innerHTML=content;  
+   itemContainer.querySelector("#name").style.background=backgroundColor;
 }
 
 function init(){
     toogleButton.addEventListener("click", toogle);
-  
+    fetchData();
  }
 
  init();
