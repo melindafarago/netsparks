0e8cfe690891a99d18d2e3b789ebd7c8c01dede4
Faragó Melinda <melinda.farago@pazirik.hu>
2023-04-27 12:36:32 +0200

    create canvas and toogle button

---
+++ src/assets/js/index.js
@@ -0,0 +1,14 @@
+let show = false;
+const showContent = () => show=!show;
+const toogleButton = document.getElementById("toogleButton");
+
+function toogle(){
+   
+}
+
+function init(){
+    toogleButton.addEventListener("click", toogle);
+  
+ }
+
+ init();
